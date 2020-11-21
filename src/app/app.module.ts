9754105485632dbbabdd3c63383467b6175fdf52
Alexander <shtaub89@gmail.com>
2020-11-21 07:27:39 +0200

    Update deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FeedbackModule } from './feedback';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
